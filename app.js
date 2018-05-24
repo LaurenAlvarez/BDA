a3d44729a44e57050ecc3ecdacb0988ec3903d3b
LaurenAlvarez <LaurenAlvarez@10.27.198.185>
2018-05-24 16:12:07 -0700

    New Server

--- app.js
+++ app.js
@@ -20,9 +20,32 @@ var express = require('express'),
       }
     ))
     app.use(express.static(__dirname + '/public'))
+    var bodyParser = require('body-parser')
+	app.use( bodyParser.json() );       // to support JSON-encoded bodies
+	app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	  extended: true
+	})); 
+	app.use(express.json());       // to support JSON-encoded bodies
+	app.use(express.urlencoded()); // to support URL-encoded bodies
     app.get('/', function (req, res) {
         res.render('index',
         { title : 'Home' }
         )
     })
+    app.get('/results', function (req, res) {
+        res.render('results',
+        { title : 'Results' }
+        )
+    })
+    app.post('/search', function(req, res){
+    		console.log(req.body)
+    		res.send(200)
+    })
+    
+    
     app.listen(3000)
+    
+    console.log("Server started...")
+    
+    
+    
---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+$("#search")
+.click(function (){
+	let topic = $("#topic").val()
+	$.post("/search", {topic: topic})
+	.done(function(){
+		window.location = "/results"
+	})
+})
