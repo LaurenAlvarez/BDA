60d159a843bb4a185dab5fa0e8ff52a709e6c67b
LaurenAlvarez <LaurenAlvarez@10.27.198.185>
2018-06-07 12:57:50 -0700

    working on 2 helper functions

--- app.js
+++ app.js
@@ -60,54 +60,65 @@ app.post('/search', function(req, res){
 
 
 app.get('/results', function (req, res) {
-	let linkCounter = 5
-	let textCounter = 5
-	let links = []
-	let text = []
 	let path = req.session.topic
-	var google = {
-	  query: 'cnn.com ' + path.toString(),
-	  age: 'y',
-	  limit: 5
-	};
-	scraper.search(google, function(err, url) {
-	  // This is called for each result
-	  if(err) throw err;
-	  console.log(url)
-	  if (linkCounter > 0) {
-		  links.push(url)
-	  }
-	  linkCounter--
-	  if (linkCounter == 0){
-		  let options = {
-			  uri: links[0],
-			  transform: function (body) {
-			    return cheerio.load(body);
-			  }
-		  };
-	  }
-	  rp(options)
-	   then(function(data)  {
-		  if (textCounter > 0){
-		  }
-		  textCounter--
-		  if (textCounter == 0){
-		    text.push(data("#storytext").text())
-		    let allTexts = text.concat().text
-		    res.render('results',
-		    { 
-		    	  title: 'Results',
-		    	  topic: req.session.topic,
-		    	  text:allText
-		    	})
-		    //	console.log(req.session.topic)
-		   	//console.log(path)
-		    	console.log(options.uri)
-		    	console.log(text)
-		  });
-	   }
-	});    
+	let links = urlSearcher(path)
+	res.render('results',
+	  { 
+	    title: 'Results',
+	    	topic: req.session.topic,
+	    	text: text
+	  })
+	  
 })
+
+//--------------------------------------------------------------
+// Helper Functions
+// --------------------------------------------------------------
+
+function urlSearcher(topic){
+	let linkCounter = 5
+	let links = []
+	var google = {
+			query: 'cnn.com ' + topic,
+			age: 'y',
+			limit: 5
+		};
+	while (linkCounter>0){
+		links.push()
+	}
+	scraper.search(google, function(err, url) {
+	    // This is called for each result
+	    if(err) throw err;
+	    console.log(url)
+		if (linkCounter > 0) {
+			links.push(url)
+		}
+		linkCounter--
+		if (linkCounter <= 0){
+			return links
+		}
+	});
+}
+function urlScraper(url){
+    let textCounter = 5
+    let text = []
+    let options = {
+			uri: url,
+			transform: function (body) {
+			    return cheerio.load(body);
+			}
+		};
+	rp.(options)
+	    .then(function(data)  {
+		    if (textCounter > 0){
+		    }
+		    textCounter--
+		    if (textCounter == 0){
+		        text.push(data("#storytext").text());
+		        let allTexts = text.concat().text
+		    }
+		}
+}
 
 app.listen(3000)
 
